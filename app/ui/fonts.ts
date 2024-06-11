70f92e0c10a16d7fb9e60cd05f673ed86f8d51f0
Keefe Overby <wrappedusername@gmail.com>
2024-06-11 18:06:21 -0500

    Configuring files

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Montserrat, Roboto } from 'next/font/google';
+
+export const montserrat = Montserrat({ weight: '400', subsets: ['latin'] });
+
+export const roboto = Roboto({ weight: ['400', '700'], subsets: ['latin'] });
