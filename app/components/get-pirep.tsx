90b1f4403c06ffa0f6a60e3c23ffdad37c3dc53f
Keefe Overby <wrappedusername@gmail.com>
2024-06-17 19:21:11 -0500

    configuring files

--- app/components/get-pirep.tsx
+++ app/components/get-pirep.tsx
@@ -8,24 +8,24 @@ async function GetPirep() {
     console.log(objPirep);
     return (
         <div className="flex flex-col h-fit w-full">
-                    <label className="flex flex-row h-fit w-full items-center justify-between py-4">
-                        PIREP:
-                        <button
-                            className="text-red-400 items-center h-fit w-1/4 bg-gray-900 rounded-md p-2"
-                            onClick={GetPirep}
-                        >
-                            Get PIREP
-                        </button>
-                    </label>
-                    <textarea
-                        className="w-full bg-gray-900 text-green-300 rounded-md p-2"
-                        name="pirepData"
-                        value={objPirep}
-                        readOnly
-                        rows={4}
-                        cols={40}
-                    />
-                </div>
+            <label className="flex flex-row h-fit w-full items-center justify-between py-4">
+                PIREP:
+                <button
+                    className="text-red-400 items-center h-fit w-1/4 bg-gray-900 rounded-md p-2"
+                    onClick={GetPirep}
+                >
+                    Get PIREP
+                </button>
+            </label>
+            <textarea
+                className="w-full bg-gray-900 text-green-300 rounded-md p-2"
+                name="pirepData"
+                value={objPirep}
+                readOnly
+                rows={4}
+                cols={40}
+            />
+        </div>
     );
 }
 
